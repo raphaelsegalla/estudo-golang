c5a0dbcbbb1fbf35a6504c58f2f451cd4744c439
raphaelsegalla <raphael.segalla@gmail.com>
2024-05-20 21:05:28 -0300

    Implementacao defer panic recover

---
+++ 14 - Funções Avançadas/14.6 - Panic e Recover/panic-recover.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func recuperarExecucao() {
+	fmt.Println("Tentativa de recuperar execução!")
+	if r := recover(); r != nil {
+		fmt.Println("Execução recuperada com sucesso!")
+	}
+}
+
+func alunoEstaAprovado(n1, n2 float32) bool {
+	defer recuperarExecucao()
+
+	media := (n1 + n2) / 2
+
+	if media > 6 {
+		return true
+	} else if media < 6 {
+		return false
+	}
+
+	panic("A média é exatamente 6!")
+}
+
+func main() {
+	// fmt.Println("Panic e Recover")
+	fmt.Println(alunoEstaAprovado(6, 6))
+	fmt.Println("Pós execução!")
+}
