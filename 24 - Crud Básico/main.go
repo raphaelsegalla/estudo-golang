539c6ebce1851d713769326e1bbf65472985b8fc
raphaelsegalla <raphael.segalla@gmail.com>
2024-06-17 21:56:47 -0300

    Implemetando CRUD

--- 24 - Crud Básico/main.go
+++ 24 - Crud Básico/main.go
@@ -13,6 +13,9 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/usuarios", servidor.CriarUsuraio).Methods(http.MethodPost)
+	router.HandleFunc("/usuarios", servidor.BuscarUsuraios).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.BuscarUsuraio).Methods(http.MethodGet)
+	router.HandleFunc("/usuarios/{id}", servidor.AtualizarUsuraio).Methods(http.MethodPut)
 
 	fmt.Println("Escutando na porta 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
